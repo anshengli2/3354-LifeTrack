80e5f9acbcf41c5f80fec9585d9ac834ba588bef
lmctx13 <58370350+lmctx13@users.noreply.github.com>
2020-04-20 11:05:07 -0500

    Add files via upload

--- testComparingActivities.java
+++ testComparingActivities.java
@@ -7,16 +7,20 @@ public class testComparingActivities {
 	@Test
 	public void testCompareActivities()
 	{
-		//these activities are conflicting --> returns true (there is a conflict)
-		//Activity a1 = new Activity(12, 4, 2019, 6, 00, 6, 30, "health", "work out");
-		//Activity a2 = new Activity(12, 4, 2019, 6, 15, 6, 45, "health", "work out");
 		
-		//these activities are not conflicting --> returns false (there is not a conflict)
-		Activity a3 = new Activity(12, 4, 2019, 11, 30, 12, 00, "health", "work out");
-		Activity a4 = new Activity(12, 4, 2019, 2, 30, 3, 30, "health", "work out");
+		//these activities are not conflicting --> function returns false 
+		//when this is in the assertFalse statement --> result is true
+		Activity a1 = new Activity(12, 4, 2019, 11, 30, 12, 00, "health", "work out");
+		Activity a2 = new Activity(12, 4, 2019, 2, 30, 3, 30, "health", "work out");
+		
+
+		//these activities are conflicting --> function returns true 
+		//when this is in the assertFalse statement --> result is false
+		//Activity a3 = new Activity(12, 4, 2019, 6, 00, 6, 30, "health", "work out");
+		//Activity a4 = new Activity(12, 4, 2019, 6, 15, 6, 45, "health", "work out");
 		
 		//check if two activities are conflicting
-		assertTrue("testing two activities", a3.checkConflicts(a4));
+		assertFalse("testing two activities", a1.checkConflicts(a2));
 		
 	}
 
