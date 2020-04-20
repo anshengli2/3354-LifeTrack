bfd86122efd18b4fbb161109a62726987057a13a
lmctx13 <58370350+lmctx13@users.noreply.github.com>
2020-04-20 11:54:41 -0500

    Update testDisplayDate.java

--- test_plan/testDisplayDate.java
+++ test_plan/testDisplayDate.java
@@ -6,17 +6,20 @@ import org.junit.Test;
 import static junit.framework.Assert.*;
 public class testDisplayDate 
 {
-
+		
+		//tests if the correct date is displayed
 		@Test
 		public void correctDateTest()
 		{
+			//the date we want to be displayed
 			Date date1 = new Date(2020, 4, 19);
 			displayDate d1 = new displayDate();
+			
+			//correct case
 			assertEquals("testing two dates", date1, new Date(2020, 4, 19));
 
+			//incorrect case
 			//assertEquals("testing two dates", date1, new Date(2016, 12, 3));
 			
 		}
-				// TODO Auto-generated method stub
-
 }
