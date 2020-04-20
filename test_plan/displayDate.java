e245ffc0f60311a72fca4252e20387f76462b6af
lmctx13 <58370350+lmctx13@users.noreply.github.com>
2020-04-20 11:53:24 -0500

    Update displayDate.java

--- test_plan/displayDate.java
+++ test_plan/displayDate.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 public class displayDate 
 {
+	//uses the compareTo() function to compare two dates
 	public int compareDates(Date d1, Date d2)
 	{
 		return d1.compareTo(d2);
